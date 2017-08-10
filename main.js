c36c512c716901c847550895c207bd58d11786b2
Justin Kim <jjkim928@gmail.com>
2017-08-10 16:39:54 -0400

    start new branch to work on button value feature instead

--- main.js
+++ main.js
@@ -4,13 +4,32 @@ let output = document.getElementById("output");
 output.innerHTML = operation;
 
 let buttons = document.querySelectorAll(".button");
-
+let operators = ['+', '-', '/', '*'];
 
 for (i in buttons) {
 	buttons[i].onclick = function(e) {
-		let input = e.target.innerText;
-		output.innerHTML = input;
-	}
+		let target = e.target.innerHTML;
+		number = parseInt(target);
+		output.innerHTML = number;
+		
+		console.log(number);
+		operation += number;
+		console.log(operation);
 
-	console.log(buttons[i].innerHTML);
-}
+		if (target === "AC") {
+			output.innerHTML = "0";
+			operation = 0;
+		}
+		else if (target === "=") {
+			output.innerHTML = eval(output.innerHTML);
+
+		}
+
+		else {
+			operation += number;
+		}
+	} // end of onclick function
+
+
+} // end of for loop
+
