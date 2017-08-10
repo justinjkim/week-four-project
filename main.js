48ec27e409333aa568f0bb2d40e5b722267e336b
Justin Kim <jjkim928@gmail.com>
2017-08-10 17:18:04 -0400

    took out onclick, replace with event listener

--- main.js
+++ main.js
@@ -7,30 +7,27 @@ let buttons = document.querySelectorAll(".button");
 let operators = ['+', '-', '/', '*'];
 
 for (i in buttons) {
-	buttons[i].onclick = function(e) {
+	buttons[i].addEventListener("click", function(e) {
 		let target = e.target.value;
 		console.log(target);
 
+		// if (target.value === "AC") {
+		// 	output.innerHTML = "0";
+		// 	operation = 0;
+		// }
+		// else if (target === "=") {
+		// 	output.innerHTML = eval(output.innerHTML);
 
-		
+		// }
 
-		if (target.value === "AC") {
-			output.innerHTML = "0";
-			operation = 0;
-		}
-		else if (target === "=") {
-			output.innerHTML = eval(output.innerHTML);
-
-		}
-
-		else {
-			operation += target;
-			let total = eval(operation);
-			console.log(total);
-			return total;
-		}
+		// else {
+		// 	operation += target;
+		// 	let total = eval(operation);
+		// 	console.log(total);
+		// 	return total;
+		// }
 	} // end of onclick function
-
+); // end of event listener
 
 } // end of for loop
 
